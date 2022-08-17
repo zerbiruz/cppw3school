d0c1071787d51816d118ede66a51c16a5bb49866
THEERA PALAJARE <zerbiruz@gmail.com>
2022-08-17 19:44:52 +0700

    Access element in multidimensional array

--- arrayDemo.cpp
+++ arrayDemo.cpp
@@ -9,8 +9,13 @@ int main() {
 	}	
 	cout << "Total: " << total << endl;
 
-	string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+	string cars[10] = {"Volvo", "BMW", "Ford", "Mazda"};
 	cout << cars[0] << endl;
+
+	for (int i = 0; i < sizeof(cars)/sizeof(string); i++){
+		cout << cars[i] << endl;
+	}
+	cout << endl;
 
 	cout << "cars array size: " << sizeof(cars) << ", string size: " << sizeof(string) << endl;
 
@@ -18,5 +23,24 @@ int main() {
 		cout << cars[i] << ", ";
 	}
 	cout << endl;
+
+	string letters[2][4] = {
+		{"A", "B", "C", "D"},
+		{"E", "F", "G", "H"}
+	};
+
+	cout << letters[0][0] << endl;
+
+	int rowNumber = sizeof(letters)/sizeof(letters[0]);
+	int colNumber = sizeof(letters[0])/sizeof(string);
+	cout << "Row number: " << rowNumber << "\nColumn number: " << colNumber << endl;
+
+	for (int i = 0; i < rowNumber; i++) {
+		for (int j = 0; j < colNumber; j++) {
+			cout << letters[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
 	return 0;
 }
